6ec74359a1910d651ca6f7c7fd5e593456da3fba
Osman Koras <nerepla.noreply@gmail.com>
2026-01-21 19:12:57 +0100

    fix some typos

--- community/issues.tsx
+++ community/issues.tsx
@@ -2,7 +2,7 @@ import { ExternalLink } from '@/types/pages';
 
 export const metadata = {
     kind: 'external',
-    title: 'Report an Bug',
+    title: 'Report an Issue',
     href: 'https://github.com/osmalpkoras/docs-generative-ai-integration-plugin/issues/new?template=bug_report.md',
     description: 'Report an issue with the Generative AI plugin',
     icon: 'Bug',
